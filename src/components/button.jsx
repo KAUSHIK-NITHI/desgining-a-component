473d8fbc529106cab7f7d1fd3af1d381a2f5d4af
unknown <kaushik.nithi@gmail.com>
2025-03-07 09:18:20 +0530

    updated

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,10 @@
 // write button card here
+function ViewProductButton() {
+    return (
+      <div>
+          <button>View Product</button>
+      </div>
+    )
+  }
+  
+  export default ViewProductButton;
